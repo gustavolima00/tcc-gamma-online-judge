5f491683a1c13be84f1fe2b37d237952523d39f3
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-06-25 17:16:39 -0300

    Correction of convert_images.sh script

--- convert_images.sh
+++ convert_images.sh
@@ -1,5 +1,10 @@
 figures_path="latex/figuras"
+editable_path="latex/editaveis"
 for image_path in $(ls $figures_path/*.png); do
     image_name=$(basename -s .png $image_path)
     convert $figures_path/$image_name.png eps2:$figures_path/$image_name.eps
+done
+for file_path in $(ls $editable_path/*.tex); do
+    sed "s/.png/.eps/g" $file_path > tmp
+    mv tmp $file_path
 done
